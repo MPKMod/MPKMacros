cad796c2cd8f8a2ef8b59c118ebc821861d165ed
Zpiboo <zpiboo@outlook.com>
2025-11-28 21:49:50 +0100

    feat: add macro warning label

--- src/main/java/io/github/kurrycat/mpkmod/module/macros/MPKMacros.java
+++ src/main/java/io/github/kurrycat/mpkmod/module/macros/MPKMacros.java
@@ -1,15 +1,21 @@
 package io.github.kurrycat.mpkmod.module.macros;
 
 import io.github.kurrycat.mpkmod.compatibility.API;
+import io.github.kurrycat.mpkmod.compatibility.MCClasses.FontRenderer;
 import io.github.kurrycat.mpkmod.compatibility.MCClasses.InputConstants;
 import io.github.kurrycat.mpkmod.compatibility.MCClasses.Keyboard;
+import io.github.kurrycat.mpkmod.compatibility.MCClasses.Renderer2D;
+import io.github.kurrycat.mpkmod.events.Event;
 import io.github.kurrycat.mpkmod.events.EventAPI;
+import io.github.kurrycat.mpkmod.events.OnRenderOverlayEvent;
 import io.github.kurrycat.mpkmod.module.macros.macro_gui.MacroGUI;
 import io.github.kurrycat.mpkmod.module.macros.util.FileUtil;
 import io.github.kurrycat.mpkmod.modules.MPKModule;
+import io.github.kurrycat.mpkmod.util.Vector2D;
 import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
 
+import java.awt.*;
 import java.util.List;
 
 
@@ -44,5 +50,24 @@ public class MPKMacros implements MPKModule {
                 currentMacro = null;
             }
         }));
+
+        final String MACRO_STRING = "MACRO";
+        final double FONT_SIZE = 18;
+        final Vector2D PERCENT_POS = new Vector2D(0.5, 0.4);
+        EventAPI.addListener(new EventAPI.EventListener<OnRenderOverlayEvent>(
+                e -> {
+                    if (currentMacro == null) return;
+
+                    Vector2D STRING_SIZE = FontRenderer.getStringSize(MACRO_STRING, FONT_SIZE);
+                    FontRenderer.drawString(
+                            MACRO_STRING,
+                            Renderer2D.getScaledSize()
+                                    .mult(PERCENT_POS)
+                                    .sub( STRING_SIZE.div(2) ),
+                            new Color(255, 75, 75), FONT_SIZE, true
+                    );
+                },
+                Event.EventType.RENDER_OVERLAY
+        ));
     }
 }
